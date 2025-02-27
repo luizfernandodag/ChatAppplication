390c351ca4040320d655603fd3ea77868b391f50
luizfernandodag <luizfernandodag@gmail.com>
2025-02-26 21:12:33 -0300

    second commit

---
+++ src/ChatClient.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 12345;
+
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
+
+            new Thread(() -> {
+                String message; 
+                try {
+                    
+                    
+                } catch (IOException e) {
+                }
+
+            }).start();
+
+
+
+             }
+             
+             catch (IOException ex) {
+        }
+             
+
+
+       
+        
+    }
+
+
+}
